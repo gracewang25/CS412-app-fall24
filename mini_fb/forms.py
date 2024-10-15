4feb8b83b35f51330ea38ccede5a12412616f5b7
Grace Wang <grace25@bu.edu>
2024-10-15 19:40:33 -0400

    fixed typo

--- mini_fb/forms.py
+++ mini_fb/forms.py
@@ -7,7 +7,7 @@ from .models import Profile, StatusMessage
 class CreateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['first_name', 'last_name', 'city', 'email_address' 'profile_image_url']
+        fields = ['first_name', 'last_name', 'city', 'email_address', 'profile_image_url']
 
 class CreateStatusMessageForm(forms.ModelForm):
     class Meta:
