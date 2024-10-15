17cf15d050bf7d7afa3db301c5655c8ada9a839a
Grace Wang <grace25@bu.edu>
2024-10-15 19:29:56 -0400

    modified settings.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -94,21 +94,22 @@ WSGI_APPLICATION = 'cs412.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-import dj_database_url
+# import dj_database_url
 
 # DATABASES = {
 #     'default': dj_database_url.config(default='postgres://localhost')
 # }
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+# }
 
 
 # Password validation
